0ddb5d67725214756171edf0d553cd6663986343
Rachid Boukaiba <rachidd@protonmail.com>
2023-03-24 15:06:00 +0100

    added WasmRunner.cs

---
+++ NUnitOnWasm/SampleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace NUnitOnWasm;
+
+[TestFixture]
+public class SampleTests
+{
+
+    [Test]
+    public void Hello()
+    {
+        Assert.Fail("Test message 12345");
+    }
+}
